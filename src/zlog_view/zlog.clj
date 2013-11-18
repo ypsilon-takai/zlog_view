5862fb524a2371f44018abfdb087f9f7f91bb7c1
ypsilon.takai <ypsilon.takai@gmail.com>
2013-11-18 19:32:32 +0900

    create command-and-read macro

--- src/zlog_view/zlog.clj
+++ src/zlog_view/zlog.clj
@@ -1,39 +1,86 @@
 (ns zlog-view.zlog
   (require [serial-port :as sp]))
 
-;; connet control
-(defn- connect [port-id]
-  (try
-    (sp/open port-id)
-    (catch Exception e false)))
+;; connect control
+(defmacro with-connection [port-id & body]
+  `(let [~'conn (sp/open ~port-id)]
+     (try
+       ~@body
+       (finally
+         (sp/close ~'conn)))))
 
-(defn- disconnect [port]
-  (sp/close port))
+(defmacro command-and-read [port-id & commands]
+  `(let [~'conn (sp/open ~port-id)
+         buff# (atom [])]
+     (try
+       (sp/on-byte ~'conn #(swap! buff# conj %))
+       ~@commands
+       (Thread/sleep 10000)
+       (into [] @buff#)
+       (finally
+         (sp/remove-listener ~'conn)
+         (sp/close ~'conn)))))
 
+(defmacro command-and-read [port-id & commands]
+  `(let [~'conn (sp/open ~port-id)
+         buff# (atom [])]
+     (try
+       (sp/on-byte ~'conn #(swap! buff# conj %))
+       ~@commands
+       (loop [last-count# (count @buff#)]
+         (Thread/sleep 3000)
+         (when (> (count @buff#) last-count#)
+           (recur (count @buff#))))
+       (into [] @buff#)
+       (finally
+         (sp/remove-listener ~'conn)
+         (sp/close ~'conn)))))
+
+
+(defn read-data
+  ([port]
+     (read-data port 3))
+  ([port timeout]
+     (let [buff (atom [])]
+       )))
 
 ;; data send/receive
-(def command-format
-  {:dataset \s
-   :getdata \a
-   :version \v
-   :erase \x
-   :reboot \R
-   :reset \*})
+(defn make-command [type & nums]
+  (->> (condp = type
+        :dataset [\s] 
+        :version [\v]
+        :erase [\x]
+        :reboot [\R]
+        :reset [\*]
+        :getdata (concat [\a] nums))
+      (map int ,,)))
 
 (defn- send-command [port command & args]
-  (let [command-char (int (get command-format command))
-        command-seq (if (= command :getdata)
-                      (->> args
-                           (map int ,,)
-                           (apply conj [command-char] ,,))
-                      [command-char])]
-    (sp/write-int-seq port command-seq)))
+  (sp/write-int-seq port (apply make-command command args )))
 
 (defn- read-data [port])
 
-(defn get-dataset [port-id]
-  (if-let [port (connect port-id)]
-    (do 
-      (send-command port :dataset)
-      (read-data port))
-    false))
+(defn get-dataset [port-name]
+  (let [output (command-and-read port-name
+                                 (send-command conn :dataset))
+        output-str (clojure.string/split (apply str (map char output)) #"\r\n")]
+    output-str
+    ))
+
+(defn get-dataset [port-name]
+  (let [output-lines (->> (command-and-read port-name
+                                            (send-command conn :dataset))
+                          (map char ,,)
+                          (apply str ,,)
+                          (#(clojure.string/split % #"\r\n*") ,,))]
+    (for [line output-lines :when (re-matches #"\d+,\d+" line)]
+      (-> (clojure.string/split line #",")
+          ((fn [[ r l]] (vector (Integer/parseInt r) l)) ,,)))))
+
+(defn get-dataset [port-name]
+  (let [output-lines (->> (command-and-read port-name
+                                            (send-command conn :dataset))
+                          (map char ,,)
+                          (apply str ,,)
+                          (#(clojure.string/split % #"\r") ,,))]
+    output-lines))
