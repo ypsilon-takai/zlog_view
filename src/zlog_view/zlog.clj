6ef99819188b54d8e7796c9ec6f8c090f0daad61
ypsilon.takai <ypsilon.takai@gmail.com>
2013-11-19 19:31:24 +0900

    big step - check functionality with actual zlog - add all perl scripts commands

--- src/zlog_view/zlog.clj
+++ src/zlog_view/zlog.clj
@@ -2,25 +2,6 @@
   (require [serial-port :as sp]))
 
 ;; connect control
-(defmacro with-connection [port-id & body]
-  `(let [~'conn (sp/open ~port-id)]
-     (try
-       ~@body
-       (finally
-         (sp/close ~'conn)))))
-
-(defmacro command-and-read [port-id & commands]
-  `(let [~'conn (sp/open ~port-id)
-         buff# (atom [])]
-     (try
-       (sp/on-byte ~'conn #(swap! buff# conj %))
-       ~@commands
-       (Thread/sleep 10000)
-       (into [] @buff#)
-       (finally
-         (sp/remove-listener ~'conn)
-         (sp/close ~'conn)))))
-
 (defmacro command-and-read [port-id & commands]
   `(let [~'conn (sp/open ~port-id)
          buff# (atom [])]
@@ -37,15 +18,8 @@
          (sp/close ~'conn)))))
 
 
-(defn read-data
-  ([port]
-     (read-data port 3))
-  ([port timeout]
-     (let [buff (atom [])]
-       )))
-
 ;; data send/receive
-(defn make-command [type & nums]
+(defn- make-command [type & nums]
   (->> (condp = type
         :dataset [\s] 
         :version [\v]
@@ -58,29 +32,64 @@
 (defn- send-command [port command & args]
   (sp/write-int-seq port (apply make-command command args )))
 
-(defn- read-data [port])
+;; exported funcs
+(defn get-device-info [port-name]
+  (->> (command-and-read port-name
+                         (send-command conn :version))
+       (map char ,,)
+       (apply str ,,)
+       (#(clojure.string/split % #"\r\n*") ,,)))
 
-(defn get-dataset [port-name]
-  (let [output (command-and-read port-name
-                                 (send-command conn :dataset))
-        output-str (clojure.string/split (apply str (map char output)) #"\r\n")]
-    output-str
-    ))
+(defn erase-data [port-name]
+  (->> (command-and-read port-name
+                         (send-command conn :erase))
+       (map char ,,)
+       (apply str ,,)
+       (#(clojure.string/split % #"\r\n*") ,,)))
 
-(defn get-dataset [port-name]
+(defn reboot-device [port-name]
+  (->> (command-and-read port-name
+                         (send-command conn :reboot))
+       (map char ,,)
+       (apply str ,,)
+       (#(clojure.string/split % #"\r\n*") ,,))  )
+
+(defn factory-reset-device [port-name]
+  (->> (command-and-read port-name
+                         (send-command conn :reset))
+       (map char ,,)
+       (apply str ,,)
+       (#(clojure.string/split % #"\r\n*") ,,))  )
+
+
+(defn get-list [port-name]
   (let [output-lines (->> (command-and-read port-name
                                             (send-command conn :dataset))
                           (map char ,,)
                           (apply str ,,)
                           (#(clojure.string/split % #"\r\n*") ,,))]
     (for [line output-lines :when (re-matches #"\d+,\d+" line)]
-      (-> (clojure.string/split line #",")
-          ((fn [[ r l]] (vector (Integer/parseInt r) l)) ,,)))))
+      (->> (clojure.string/split line #",")
+           (map #(Integer/parseInt %) ,,)
+           ((fn [[idx cnt]] {:idx idx :data-count cnt}) ,, )))))
 
-(defn get-dataset [port-name]
-  (let [output-lines (->> (command-and-read port-name
-                                            (send-command conn :dataset))
-                          (map char ,,)
-                          (apply str ,,)
-                          (#(clojure.string/split % #"\r") ,,))]
-    output-lines))
+
+(defn- destruct-dataset [byte-seq]
+  (let [[_ rate1 rate2 samples1 samples2 trigger & alt-data] byte-seq]
+    {:rate (+ (* rate1 0xff) rate2)
+     :sample-count (+ (* samples1 0xff) samples2)
+     :trigger trigger
+     :alt-data (->> alt-data
+                    (partition 2 ,,) 
+                    (map (fn [[l r]] (+ (* l 0xff) r)) ,,) ; byte * 2 -> short
+                    (map #(if (>= % 0x8000) (+ (- 0xffff %) 1) %) ,,)) ; unsigend short -> signed
+     }))
+
+(defn get-dataset [port-name data-idx]
+  (let [output-bytes (-> (command-and-read port-name
+                                           (send-command conn :getdata data-idx))
+                          )]
+    (->> output-bytes
+         (drop-while #(not= % 0x80),,)
+         (destruct-dataset ,,))))
+
