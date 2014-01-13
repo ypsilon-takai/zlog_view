01053a0943674f92a693ba53e5bcc43b357840d8
ypsilon.takai <ypsilon.takai@gmail.com>
2014-01-13 21:19:56 +0900

    going foreward

--- src/zlog_view/zlog.clj
+++ src/zlog_view/zlog.clj
@@ -3,7 +3,7 @@
 
 (def ^:dynamic *data-timeout* 3000)
 
-;; connect control
+;; connection control
 (defmacro command-and-read [port-id & commands]
   `(let [conn# (sp/open ~port-id)
          buff# (atom [])]
@@ -20,7 +20,23 @@
          (sp/remove-listener conn#)
          (sp/close conn#)))))
 
-
+#_(defmacro command-and-read [port-id & commands]
+  `(try
+     (let [conn# (sp/open ~port-id)
+           buff# (atom [])]
+       (try
+         (sp/on-byte conn# #(swap! buff# conj %))
+         (-> conn#
+             ~@commands)
+         (loop [last-count# (count @buff#)]
+           (Thread/sleep *data-timeout*)
+           (when (> (count @buff#) last-count#)
+             (recur (count @buff#))))
+         (into [] @buff#)
+         (finally
+           (sp/remove-listener conn#)
+           (sp/close conn#))))
+     (catch Exception e [])))
 
 ;; data send/receive
 (defn- make-command [type & nums]
@@ -36,50 +52,69 @@
 (defn- send-command [port command & args]
   (sp/write-int-seq port (apply make-command command args )))
 
+
+(defn- bytes->lines [byte-seq]
+  (->> byte-seq
+       (map char ,,)
+       (apply str ,,)
+       (clojure.string/split-lines ,,)))
+
 ;; exported funcs
-(defn get-device-info [port-name]
+(defn port-names
+  []
+  (mapv #(.getName %) (sp/port-ids)))
+
+(defn get-device-info
+  "Returns a map of device information from zlog which conncet port-name.
+   Contents:
+      :name -> Device name.
+      :model -> Zlog model name.
+      :fw-ver -> Zlog firmware version.
+      :fw-date -> zlog firmware release date."
+  [port-name]
   (->> (command-and-read port-name
                          (send-command ,,, :version))
        (map char ,,)
        (apply str ,,)
-       (#(clojure.string/split % #"\r\n*") ,,)
+       (clojure.string/split-lines ,,)
        (zipmap [:name :model :fw-ver :fw-date] ,,)))
 
-(defn erase-data [port-name]
-  (->> (command-and-read port-name
-                         (send-command ,,, :erase))
-       (map char ,,)
-       (apply str ,,)
-       (#(clojure.string/split % #"\r\n*") ,,)))
+(defn erase-data
+  "Erase all data in connected zlog."
+  [port-name]
+  (-> (command-and-read port-name
+                        (send-command ,,, :erase))
+      (bytes->lines)))
 
-(defn reboot-device [port-name]
-  (->> (command-and-read port-name
+(defn reboot-device
+  "Power on and off connected zlog to make new configuration active."
+  [port-name]
+  (-> (command-and-read port-name
                          (send-command ,,, :reboot))
-       (map char ,,)
-       (apply str ,,)
-       (#(clojure.string/split % #"\r\n*") ,,))  )
+      (bytes->lines)))
 
-(defn factory-reset-device [port-name]
-  (->> (command-and-read port-name
+(defn factory-reset-device
+  "Reset device to factory configurations."
+  [port-name]
+  (-> (command-and-read port-name
                          (send-command ,,, :reset))
-       (map char ,,)
-       (apply str ,,)
-       (#(clojure.string/split % #"\r\n*") ,,))  )
+      (bytes->lines)))
 
-
-(defn get-list [port-name]
-  (let [output-lines (->> (command-and-read port-name
+(defn get-list
+  "Get data indices which stores in the connected zlog"
+  [port-name]
+  (let [output-lines (-> (command-and-read port-name
                                             (send-command ,,, :dataset))
-                          (map char ,,)
-                          (apply str ,,)
-                          (#(clojure.string/split % #"\r\n*") ,,))]
+                         (bytes->lines))]
     (for [line output-lines :when (re-matches #"\d+,\d+" line)]
       (->> (clojure.string/split line #",")
            (map #(Integer/parseInt %) ,,)
-           ((fn [[idx cnt]] {:idx idx :data-count cnt}) ,, )))))
+           (zipmap [:idx :aux] ,, )))))
 
 
-(defn- destruct-dataset [byte-seq]
+(defn- destruct-dataset
+  "Read byte sequences and translate them into altitude detas."
+  [byte-seq]
   (let [[_ rate1 rate2 samples1 samples2 trigger & alt-data] byte-seq
         sample-count (let [count-in (+ (* samples1 0xff) samples2)]
                        (if (pos? count-in)
@@ -91,12 +126,14 @@
      :trigger trigger
      :alt-data (->> alt-data
                     (partition 2 ,,) 
-                    (map (fn [[l r]] (+ (* l 0xff) r)) ,,) ; byte * 2 -> short
-                    #_(map #(if (>= % 0x8000) (+ (- 0xffff %) 1) %) ,,)
-                    (take sample-count ,,))                 ; unsigend short -> signed
+                    (map (fn [[l r]] (+ (* l 0x100) r)) ,,) ; byte * 2 -> short
+                    (map #(if (>= % 0x8000) (+ (- 0xffff %) 1) %) ,,) ; unsigend short -> signed
+                    (take sample-count ,,))                 
      }))
 
-(defn get-dataset [port-name data-idx]
+(defn get-dataset
+  "Get altitude data which specified by index from zlog and returans altitude data seq"
+  [port-name data-idx]
   (let [output-bytes (-> (command-and-read port-name
                                            (send-command ,,, :getdata data-idx))
                           )]
